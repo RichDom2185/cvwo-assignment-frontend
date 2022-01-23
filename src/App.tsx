c87ec00345d9a9ec432fd8b6fd96982800bf8320
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-01-23 14:42:24 +0800

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,6 @@ function App() {
         <Route path="/details" element={<DetailsPage />} />
       </Routes>
     </Router>
-    // <div className="App">
-    //   <Header />
-    //   <MoreButton />
-    //   <header className="App-header">
-    //     <img src="https://source.unsplash.com/random" className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save this file to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
 
   );
 }
--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -51,31 +51,9 @@ const LoginForm = () => {
                 </div>
             </div>
             <form onSubmit={handleSubmit}>
-                {/* <div className="flex flex-col space-y-2">
-                    <label htmlFor="email" className="flex-grow text-left text-gray-500 tracking-wide font-medium text-sm">E-mail</label>
-                    <input type="email" name="email" id="email" value={form.email} onChange={handleChange} className="transition border border-transparent focus:border-blue-300 bg-gray-100 focus:bg-sky-100 rounded px-3 py-2 w-full focus:outline-none" />
-                </div> */}
                 <FormTextInputElement name="email" label="E-mail" value={form.email} placeholder="Enter your email" changeHandler={handleChange} />
                 <FormTextInputElement name="password" label="Password" value={form.password} placeholder="Enter your password" changeHandler={handleChange} />
-                {/* <div className="flex items-center justify-start">
-                <label htmlFor="password" className="font-bold text-gray-500 basis-1/6 text-right pr-4 max-w-p-100">Password</label>
-                <input type="password" name="password" id="password" value={form.password} onChange={handleChange} className="transition bg-gray-100 focus:bg-sky-100 rounded px-3 py-2 flex-grow outline-1 focus:outline-blue-500"/>
-            </div> */}
                 <button type="submit" className="mt-10 focus:outline-none font-bold transition bg-white hover:bg-blue-700 focus:bg-blue-700 hover:text-white focus:text-white shadow-lg hover:shadow-blue-200 focus:shadow-blue-200 px-3 py-2 rounded-lg w-full">Login</button>
-                {/* <div className="separator flex justify-center items-center">
-                    <span className="mx-2">
-                        or
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        Sign in with:
-                    </span>
-                    <div>
-                        <button className="inline transition bg-gray-200 hover:bg-gray-300 px-3 py-2 rounded">Google</button>
-                        <button className="inline transition bg-gray-200 hover:bg-gray-300 px-3 py-2 rounded">GitHub</button>
-                    </div>
-                </div> */}
             </form>
         </div>
     )
