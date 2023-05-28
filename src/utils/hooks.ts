0c7f3b4cdf805538c73ebc2c2cfc92c9c754a7b6
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-28 18:45:20 +0800

    Memoize localStorage getters and setters

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,4 +1,5 @@
 import { compressToUTF16, decompressFromUTF16 } from "lz-string";
+import { useCallback } from "react";
 import { LocalStorage } from "../types/localStorage";
 
 type MakeGetterTypes<T extends Record<string, any>> = {
@@ -24,16 +25,19 @@ export const useLocalStorage = <T extends keyof LocalStorage>(
   const setterKey: `setStorage${Capitalize<T>}` = `setStorage${camelCaseKey}`;
   // TODO: Investigate weird inferred type
   const exports = {
-    [getterKey]: (): LocalStorage[T] | undefined => {
+    [getterKey]: useCallback((): LocalStorage[T] | undefined => {
       const value = window.localStorage.getItem(key);
       return value === null
         ? undefined
         : JSON.parse(decompressFromUTF16(value));
-    },
-    [setterKey]: (value: LocalStorage[T]): void => {
-      const json = JSON.stringify(value);
-      window.localStorage.setItem(key, compressToUTF16(json));
-    },
+    }, [key]),
+    [setterKey]: useCallback(
+      (value: LocalStorage[T]): void => {
+        const json = JSON.stringify(value);
+        window.localStorage.setItem(key, compressToUTF16(json));
+      },
+      [key]
+    ),
   };
   // TODO: Investigate if we can remove the typecast
   return exports as any;
