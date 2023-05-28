7d00781a64297abe022b038445e4c7f2dbeb9a04
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-29 02:37:31 +0800

    Use tab coloring utilities for TaskList filters
    Note that the component is still named Tab for now while it awaits
    renaming further into the refactoring process.

--- src/components/taskView/Tab.tsx
+++ src/components/taskView/Tab.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { colorFromTag, generateColorClasses } from "../../utils/tag";
 
 interface Props {
   tabName: string;
@@ -13,81 +14,10 @@ const Tab: React.FC<Props> = ({
   isColored = false,
   callback = () => {},
 }) => {
-  const colors: string[] = [
-    "gray",
-    "red",
-    "orange",
-    "amber",
-    "yellow",
-    "lime",
-    "green",
-    "emerald",
-    "teal",
-    "cyan",
-    "sky",
-    "blue",
-    "indigo",
-    "violet",
-    "purple",
-    "fuchscia",
-    "pink",
-    "rose",
-  ];
-
-  const generator = require("random-seed");
-
-  function colorFromTag(tag: string): string {
-    const index: number = generator(tag).intBetween(0, colors.length - 1);
-    return colors[index];
-  }
-
-  function generateColorClasses(color: string): string {
-    switch (color) {
-      case "gray":
-        return " bg-gray-100";
-      case "red":
-        return " bg-red-100";
-      case "orange":
-        return " bg-orange-100";
-      case "amber":
-        return " bg-amber-100";
-      case "yellow":
-        return " bg-yellow-100";
-      case "lime":
-        return " bg-lime-100";
-      case "green":
-        return " bg-green-100";
-      case "emerald":
-        return " bg-emerald-100";
-      case "teal":
-        return " bg-teal-100";
-      case "cyan":
-        return " bg-cyan-100";
-      case "sky":
-        return " bg-sky-100";
-      case "blue":
-        return " bg-blue-100";
-      case "indigo":
-        return " bg-indigo-100";
-      case "violet":
-        return " bg-violet-100";
-      case "purple":
-        return " bg-purple-100";
-      case "fuchsia":
-        return " bg-fuchsia-100";
-      case "pink":
-        return " bg-pink-100";
-      case "rose":
-        return " bg-rose-100";
-      default:
-        return "";
-    }
-  }
-
   return (
     <li
       className={
-        "px-2 rounded bg-gray-100 text-gray-600 cursor-pointer select-none" +
+        "px-2 rounded bg-gray-100 text-gray-600 cursor-pointer select-none " +
         (isColored ? generateColorClasses(colorFromTag(tabName)) : "") +
         (disabled
           ? ""
